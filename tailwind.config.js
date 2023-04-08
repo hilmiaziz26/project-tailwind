08adda120b080a8541123ca5df5a73cbcbed95ba
hilmiaziz26 <hilminurulaziz@gmail.com>
2023-04-08 21:43:39 +0700

    added primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fbbf24'
+      },
+    },
   },
   plugins: [
     require('tailwind-scrollbar-hide')
